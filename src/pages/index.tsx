6a985da84078df08474bb37eb3a66a33e01e35ab
arosh-segar <arosh.segar@gmail.com>
2022-05-23 00:02:16 +0530

    meta information added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { Header } from "../components/Homepage/Header";
 
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <>
+      <Head>
+        <title>Finance Empire</title>
+        <meta name="title" content="Finance Empire" />
+        <meta
+          name="description"
+          content="An application to efficiently manage your expenses"
+        />
+        <meta property="og:title" content="Finance Empire" />
+        <meta
+          property="og:description"
+          content="An application to efficiently manage your expenses"
+        />
+        <meta
+          property="og:description"
+          content="An application to efficiently manage your expenses"
+        />
+        <meta property="og:image" content="/header.png" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </>
+  );
 };
 
 export default Home;
