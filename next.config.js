1314c2a06e56d57221ec6837399ed01d77e0964c
arosh-segar <arosh.segar@gmail.com>
2022-05-24 15:55:25 +0530

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: [...API_URL],
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
